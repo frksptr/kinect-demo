39c9ef9e936ca573d8344d6ddfe532ccd13c6c6d
Farkas Peter <frkspeter@gmail.com>
2015-07-09 14:02:36 +0200

    nocommit

--- KinectDemoSGL/obj/x86/Debug/MainWindow.g.cs
+++ KinectDemoSGL/obj/x86/Debug/MainWindow.g.cs
@@ -9,7 +9,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using KinectDemoSGL.Util;
 using System;
 using System.Diagnostics;
 using System.Windows;
