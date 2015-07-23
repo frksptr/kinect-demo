55690ede739e8709799846f76ebe7aa378bf0147
Farkas Peter <frkspeter@gmail.com>
2015-07-23 10:09:14 +0200

    debugging workspace 3d vertices

--- KinectDemoSGL/UIElement/RoomPointCloudView.xaml.cs
+++ KinectDemoSGL/UIElement/RoomPointCloudView.xaml.cs
@@ -130,10 +130,30 @@ namespace KinectDemoCommon.UIElement
                 gl.End();
 
                 gl.Begin(OpenGL.GL_TRIANGLES);
-                gl.Color(0 - 0f, 1.0f, 0.0f);
+                gl.Color(0.0f, 1.0f, 0.0f);
                 foreach (Workspace workspace in DataStore.Instance.WorkspaceDictionary.Values)
                 {
                     Point3D[] vertices = workspace.FittedVertices;
+                    Point3D v0 = vertices[0];
+                    Point3D v1 = vertices[1];
+                    Point3D v2 = vertices[2];
+                    Point3D v3 = vertices[3];
+                    gl.Vertex(v0.X, v0.Y, v0.Z);
+                    gl.Vertex(v1.X, v1.Y, v1.Z);
+                    gl.Vertex(v2.X, v2.Y, v2.Z);
+
+                    gl.Vertex(v2.X, v2.Y, v2.Z);
+                    gl.Vertex(v3.X, v3.Y, v3.Z);
+                    gl.Vertex(v0.X, v0.Y, v0.Z);
+
+                }
+                gl.End();
+
+                gl.Begin(OpenGL.GL_TRIANGLES);
+                gl.Color(0.0f, 0.0f, 1.0f);
+                foreach (Workspace workspace in DataStore.Instance.WorkspaceDictionary.Values)
+                {
+                    Point3D[] vertices = workspace.Vertices3D;
                     Point3D v0 = vertices[0];
                     Point3D v1 = vertices[1];
                     Point3D v2 = vertices[2];
