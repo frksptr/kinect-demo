736de4bac3aab2f6b7cb841787f5295f48f0a557
Farkas Peter <frkspeter@gmail.com>
2015-07-29 16:32:20 +0200

    pcd save format fix

--- KinectDemoCommon/Util/FileHelper.cs
+++ KinectDemoCommon/Util/FileHelper.cs
@@ -13,16 +13,16 @@ namespace KinectDemoCommon.Util
     {
         private static string pcdHeader =
             @"# .PCD v.7 - Point Cloud Data file format
-            VERSION 0.7
-            FIELDS x y z
-            SIZE 4 4 4
-            TYPE F F F
-            COUNT 1 1 1
-            WIDTH numberofpoints
-            HEIGHT 1
-            VIEWPOINT 0 0 0 1 0 0 0
-            POINTS numberofpoints
-            DATA ascii";
+VERSION 0.7
+FIELDS x y z
+SIZE 4 4 4
+TYPE F F F
+COUNT 1 1 1
+WIDTH numberofpoints
+HEIGHT 1
+VIEWPOINT 0 0 0 1 0 0 0
+POINTS numberofpoints
+DATA ascii";
 
         public static void WritePCD(List<Point3D> points, string path)
         {
@@ -32,9 +32,10 @@ namespace KinectDemoCommon.Util
             using (StreamWriter sw = new StreamWriter(fs))
             {
                 sw.Write(header);
+                sw.Write('\n');
                 foreach (Point3D point in points)
                 {
-                    sw.WriteLine((point.X + " " + point.Y + " " + point.Z));
+                    sw.WriteLine((point.X + " " + point.Y + " " + point.Z).Replace(",","."));
                 }
             }
         }
